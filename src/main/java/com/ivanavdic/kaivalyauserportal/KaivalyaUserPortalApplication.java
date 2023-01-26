1ce30c7ccf064ab68aa1be21350137aed8d73baf
Ivan Avdic <ivan.avdic.sp@gmail.com>
2023-01-26 18:44:46 +0100

    Spring Initializr files

---
+++ src/main/java/com/ivanavdic/kaivalyauserportal/KaivalyaUserPortalApplication.java
@@ -0,0 +1,13 @@
+package com.ivanavdic.kaivalyauserportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaivalyaUserPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaivalyaUserPortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ivanavdic/kaivalyauserportal/KaivalyaUserPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivanavdic.kaivalyauserportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaivalyaUserPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
